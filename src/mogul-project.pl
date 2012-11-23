79bec93776ca8db587054f95f4cb8df47ca01890
Alan James <alanjames1987@gmail.com>
2012-11-23 12:58:19 -0500

    mogu-project now accepts one argument, the name of the project.

--- src/mogul-project.pl
+++ src/mogul-project.pl
@@ -2,7 +2,13 @@
 
 use Term::ANSIColor;
 
-system('tar xvzf /opt/mogul/Mogul.tar.gz');
+if ( @ARGV[0] ) {
+	mkdir @ARGV[0];
+	system( 'tar xvzf /opt/mogul/Mogul.tar.gz -C ' . @ARGV[0] );
+}
+else {
+	system('tar xvzf /opt/mogul/Mogul.tar.gz');
+}
 
 print color 'green';
 print "complete\n";
